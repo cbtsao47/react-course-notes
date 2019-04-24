1f275fafc0e1314182c90936d2da47c732ce02db
wisdomtooth <cbtsao47@gmail.com>
2019-04-24 00:04:11 -0500

    add die style

--- yahtzee-starter/src/Die.js
+++ yahtzee-starter/src/Die.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numWords: ["one", "two", "three", "four", "five", "six"]
+  };
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
@@ -10,15 +13,11 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
+    let classes = `Die fas fa-dice-${
+      this.props.numWords[this.props.val - 1]
+    } fa-5x `;
+    if (this.props.locked) classes += "Die-locked";
+    return <i className={classes} onClick={this.handleClick} />;
   }
 }
 
