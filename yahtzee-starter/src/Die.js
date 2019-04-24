c1e2d7bdcb17d1e373fed32d0f10551143acc0f9
wisdomtooth <cbtsao47@gmail.com>
2019-04-24 00:22:48 -0500

    complete yahtzee

--- yahtzee-starter/src/Die.js
+++ yahtzee-starter/src/Die.js
@@ -13,10 +13,9 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    let classes = `Die fas fa-dice-${
-      this.props.numWords[this.props.val - 1]
-    } fa-5x `;
-    if (this.props.locked) classes += "Die-locked";
+    const { numWords, locked, val } = this.props;
+    let classes = `Die fas fa-dice-${numWords[val - 1]} fa-5x `;
+    if (locked) classes += "Die-locked";
     return <i className={classes} onClick={this.handleClick} />;
   }
 }
